afb007e8b5db636c39bc946fa5e0f57bffd9e295
Cherlia Fleming <cherlia91@gmail.com>
2026-07-28 23:38:46 -0500

    Added Farm.css and Rest.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
 import './App.css'
 import { FarmPickDrop } from './components/FarmPickDrop.jsx'
-
+import { RestPickDrop } from './components/RestPickDrop.jsx'
 export const App = () => {
   return (
     <div className="App">
       <FarmPickDrop />
+      <RestPickDrop />
     </div>
   )
 }
