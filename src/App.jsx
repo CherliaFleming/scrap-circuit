c7dc46111e90c397b54afb293f53c19a6f61b863
Cherlia Fleming <cherlia91@gmail.com>
2026-07-31 00:22:33 -0500

    Created Login.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,17 @@ import './App.css'
 import { FarmPickDrop } from './components/FarmPickDrop.jsx'
 import { RestPickDrop } from './components/RestPickDrop.jsx'
 import MainView from './views/MainView.jsx' //default export no curly braces
+import { Routes, Route } from 'react-router-dom'
+
 
 export const App = () => {
   return (
     <div className="App">
-      <FarmPickDrop />
-      <RestPickDrop />
+    <Routes>
+     <Route path="/" element={<MainView />}/>
+     <Route path="/farmer" element={<FarmPickDrop />}/>
+     <Route path="/restaurant" element={<RestPickDrop />}/>
+    </Routes>
     </div>
   )
 }
-export default App; 
---
+++ src/forms/Login.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react';
+
+const Login = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleEmailChange = (event) => {
+        setEmail(event.target.value);
+    };
+
+    const handlePasswordChange = (event) => {
+        setPassword(event.target.value);
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log('Email:', email);
+        console.log('Password:', password);
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <label>
+                Email:
+                <input type="email" value={email} onChange={handleEmailChange} />
+            </label>
+            <br />
+            <label>
+                Password:
+                <input type="password" value={password} onChange={handlePasswordChange} />
+            </label>
+            <br />
+            <button type="submit">Login</button>
+        </form>
+    );
+};
