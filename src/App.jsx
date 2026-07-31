a7e824b446f70afe159a8a34acdc3a23dfba07d2
Cherlia Fleming <cherlia91@gmail.com>
2026-07-30 22:55:28 -0500

    Added MainView.jsx and adding Routes wrap to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import './App.css'
 import { FarmPickDrop } from './components/FarmPickDrop.jsx'
 import { RestPickDrop } from './components/RestPickDrop.jsx'
+import MainView from './views/MainView.jsx' //default export no curly braces
+
 export const App = () => {
   return (
     <div className="App">
