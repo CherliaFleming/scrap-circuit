a03a8ffeeb3587bf8c215a36d95d38fa7f529213
Cherlia Fleming <cherlia91@gmail.com>
2026-08-01 01:28:42 -0500

    Just tested logging in and page renders, title schedules and logout button all appears and works.

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,14 @@ import MainView from './views/MainView.jsx' //default export no curly braces
 import { Routes, Route } from 'react-router-dom'
 import { Login } from './forms/Login.jsx'
 import { Register } from './forms/Register.jsx'
+import { FarmView } from './views/FarmView.jsx'
 
 export const App = () => {
   return (
     <div className="App">
     <Routes>
      <Route path="/" element={<MainView />}/>
-     <Route path="/farmer" element={<FarmPickDrop />}/>
+     <Route path="/farmer" element={<FarmView />}/>
      <Route path="/restaurant" element={<RestPickDrop />}/>
      <Route path="/login" element={<Login />} />
      <Route path="/register" element={<Register />} />
