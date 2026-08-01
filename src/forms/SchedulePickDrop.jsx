97d253ae0a11f5672029ac7cc1ef21637da6f0aa
Cherlia Fleming <cherlia91@gmail.com>
2026-08-01 12:46:25 -0500

    Worked through debugging SchedulePickDrop.jsx still working on getting form to pull up probly forgot to add to App.jsx I am working throught that. Went ahead and created a EditPickDrop.jsx and copying the same type logic for form.

--- src/forms/SchedulePickDrop.jsx
+++ src/forms/SchedulePickDrop.jsx
@@ -1,8 +1,10 @@
 import {useState} from 'react';
 import { useNavigate } from 'react-router-dom';
+import { FarmPickDrop } from '../components/FarmPickDrop';
+
 
 const SchedulePickDrop = () => {
-    const [farmerName, setFarmerName] = useState('');
+    const [farmName, setFarmName] = useState('');
     const [compostType, setCompostType] = useState('');
     const [date, setDate] = useState('');
     const [type, setType] = useState('');
@@ -16,3 +18,44 @@ const SchedulePickDrop = () => {
     const handleCompostChange = (event) => {
         setCompostType(event.target.value);
     };
+
+    const handleDateChange = (event) => {
+        setDate(event.target.value);
+    };
+
+    const handleTypeChange = (event) => {
+        setType(event.target.value);
+    };
+
+    const handleSubmit = (event) => {
+        //stops the browser's default behavior for form submission
+        event.preventDefault();
+        fetch('http://localhost:8088/schedules', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                farmName,
+                compostType,
+                date,
+                type
+            })
+        }).then(() => {
+            navigate('/schedule')
+        })
+    };
+
+    return (
+        <div>
+            <input type="text" placeholder="Farm Name" value={farmName} onChange={handleNameChange} />
+        <input type="text" placeholder="Compost Type" value={compostType} onChange={handleCompostChange} />
+        <input type="text" placeholder="Date" value={date} onChange={handleDateChange} />
+        <input type="text" placeholder="Type" value={type} onChange={handleTypeChange} />
+        <select value={type} onChange={handleTypeChange}>
+            <option value="Pick-Up">Pick-Up</option>
+            <option value="Drop-Off">Drop-Off</option>
+        </select>
+        <button type="submit">Submit</button>
+        </div>
+    )
